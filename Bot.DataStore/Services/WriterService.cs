3d551db61e523c787f402c2de2417bf29ab3c010
Врублевский Алексей <a.vrublevsky@z-it.ru>
2020-07-10 23:45:43 +0300

    [нов] - добавил проект для описания базы данных - добавил проект для обработчика сообщений телеграмм

---
+++ Bot.DataStore/Services/WriterService.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using Protos;
+
+namespace Bot.DataStore.Services
+{
+    public class WriterService : Writer.WriterBase
+    {
+        private readonly ILogger<WriterService> _logger;
+        public WriterService(ILogger<WriterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<WriteSpendingDataResponce> SaveSpendingData(WriteSpendingDataRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new WriteSpendingDataResponce());
+        }
+    }
+}
