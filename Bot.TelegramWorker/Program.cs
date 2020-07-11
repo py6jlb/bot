0e3a631d2fdc900891687375949720463b5bbb93
Врублевский Алексей <a.vrublevsky@z-it.ru>
2020-07-11 11:22:13 +0300

    [нов] - добавил инициализацию универсального узла

--- Bot.TelegramWorker/Program.cs
+++ Bot.TelegramWorker/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Bot.TelegramWorker
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await CreateHostBuilder(args).RunConsoleAsync();
+        }
+
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    var env = hostContext.HostingEnvironment;
+                    configApp.SetBasePath(Directory.GetCurrentDirectory());
+                    configApp.AddEnvironmentVariables();
+                    configApp.AddCommandLine(args);
+                })
+                .ConfigureServices((hostContext, services) => {
+                    //services.InitWorker(hostContext);
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    configLogging.AddConsole();
+                });
         }
     }
 }
